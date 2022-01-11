9dbf1289e017292a8c21109ed9100b0d975b5915
CyrussT <24277152+CyrussT@users.noreply.github.com>
2022-01-11 17:25:55 +0800

    Submission of labs week 1

---
+++ Question2a.java
@@ -0,0 +1,5 @@
+public class Question2a {
+    public static void main(String[] args) {
+        System.out.println("Hi, I am Cyrus!");
+    }
+}
---
+++ Question2c.java
@@ -0,0 +1,10 @@
+public class Question2c {
+    public static void main (String[] args) {
+        for (int i = 102; i != 66; i--) {
+            if (i % 2 == 1) {
+                System.out.print("Value of x: " + i);
+                System.out.print("\n");
+            }
+        }
+    }
+}
